6a1b691a48acef8b0aec70d84b44d0c2c02f2021
Bonnie <birchba@gmail.com>
2021-05-29 21:42:35 -0500

    code along

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,65 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+<h1>hi there</h1>
+  <div class="events">
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import EventCard from "@/components/EventCard.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    EventCard
+  },
+  data() {
+    return{
+    events: [
+      {
+        id: 5928101,
+        category: "animals",
+        title: "adoption day",
+        description: "cat cat dog",
+        location: "house",
+        date: "tuesday",
+        time: "noon",
+        petsAllowed: true,
+        organizer: "me"
+      },
+      {
+        id: 5928191,
+        category: "houses",
+        title: "donate a house item",
+        description: "yep",
+        location: "house",
+        date: "thursday",
+        time: "noonthirty",
+        petsAllowed: false,
+        organizer: "me"
+      },
+      {
+        id: 5988101,
+        category: "animals",
+        title: "snack day",
+        description: "cat dog cat",
+        location: "yard",
+        date: "friday",
+        time: "evening",
+        petsAllowed: true,
+        organizer: "fred"
+      }
+    ]
+    }
   }
 };
 </script>
+
+<style scoped>
+.events{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  }
+</style>
